4dc5d16e7a0d2567c41fee96981a1231b8aac5ae
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-09-04 19:46:48 +0300

    finish implementation

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,31 +3,52 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_js_1 = __importDefault(require("crypto-js"));
 const add_logo_1 = __importDefault(require("./add-logo"));
+const HASH_KEY = "demoCHANGED";
 const requestInitiatorValues = {
     live: 0,
-    oid: "",
-    inv: "",
-    amount: 1,
-    tel: "",
-    eml: "",
+    oid: "1",
+    inv: "1",
+    ttl: 1,
+    tel: "254717582057",
+    eml: "user@email.com",
     vid: "demo",
     curr: "KES",
     p1: "demo",
     p2: "demo",
     p3: "demo",
     p4: "demo",
-    cbk: "",
+    cbk: "http://localhost:3001",
     cst: 1,
     crl: 0,
-    hash: "",
+    hsh: "",
 };
 add_logo_1.default();
 const form = document.getElementById("iPay-Form");
 const submitBtn = document.createElement("input");
 submitBtn.type = "submit";
 submitBtn.className = "btn btn-primary";
-submitBtn.value = `Pay ${requestInitiatorValues.curr}(${requestInitiatorValues.amount})`;
+submitBtn.value = `Pay ${requestInitiatorValues.curr}(${requestInitiatorValues.ttl})`;
+const onChange = (event) => {
+    const { name, value } = event.target;
+    Object.assign(requestInitiatorValues, Object.assign(Object.assign({}, requestInitiatorValues), { [name]: value }));
+    if (name === "oid" || name === "inv") {
+        // ensure invoice and order id are the same
+        const orderId = requestInitiatorValues.oid;
+        const invoiceId = requestInitiatorValues.inv;
+        const orderIdInput = document.getElementById("id-oid");
+        const invoiceIdInput = document.getElementById("id-inv");
+        if (orderId > invoiceId) {
+            requestInitiatorValues.inv = orderId;
+            invoiceIdInput.value = orderId;
+        }
+        if (invoiceId > orderId) {
+            requestInitiatorValues.oid = invoiceId;
+            orderIdInput.value = invoiceId;
+        }
+    }
+};
 Object.keys(requestInitiatorValues).forEach((key) => {
     const group = document.createElement("div");
     group.className = "form-group";
@@ -41,10 +62,30 @@ Object.keys(requestInitiatorValues).forEach((key) => {
     input.id = `id-${key}`;
     input.name = key;
     input.className = "form-control";
+    input.addEventListener("change", (event) => {
+        onChange(event);
+    });
     group.appendChild(label);
     group.appendChild(input);
     form.appendChild(group);
     form.appendChild(submitBtn);
 });
-console.log("yeeey");
+const hashDataString = (dataString) => {
+    return crypto_js_1.default.HmacSHA1(dataString, HASH_KEY).toString();
+};
+const onSubmit = () => {
+    const dataStringObj = Object.assign({}, requestInitiatorValues);
+    delete dataStringObj.hsh;
+    const dataString = Object.keys(dataStringObj)
+        .map((key) => dataStringObj[key])
+        .join("");
+    const hash = hashDataString(dataString);
+    const hashInput = document.getElementById("id-hsh");
+    hashInput.value = hash;
+};
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    onSubmit();
+    event.target.submit();
+});
 //# sourceMappingURL=app.js.map
--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -1,4 +1,7 @@
+import CryptoJS from "crypto-js";
 import addLogo from "./add-logo";
+
+const HASH_KEY = "demoCHANGED";
 
 type RequestInitiatorType = {
   [field: string]: string | number;
@@ -6,21 +9,21 @@ type RequestInitiatorType = {
 
 const requestInitiatorValues: RequestInitiatorType = {
   live: 0,
-  oid: "",
-  inv: "",
-  amount: 1,
-  tel: "",
-  eml: "",
+  oid: "1",
+  inv: "1",
+  ttl: 1,
+  tel: "254717582057",
+  eml: "user@email.com",
   vid: "demo",
   curr: "KES",
   p1: "demo",
   p2: "demo",
   p3: "demo",
   p4: "demo",
-  cbk: "",
+  cbk: "http://localhost:3001",
   cst: 1,
   crl: 0,
-  hash: "",
+  hsh: "",
 };
 
 addLogo();
@@ -28,13 +31,40 @@ const form = document.getElementById("iPay-Form")!;
 const submitBtn = document.createElement("input");
 submitBtn.type = "submit";
 submitBtn.className = "btn btn-primary";
-submitBtn.value = `Pay ${requestInitiatorValues.curr}(${requestInitiatorValues.amount})`;
+submitBtn.value = `Pay ${requestInitiatorValues.curr}(${requestInitiatorValues.ttl})`;
+
+const onChange = (event: Event): void => {
+  const { name, value } = <HTMLInputElement>event.target;
+  Object.assign(requestInitiatorValues, {
+    ...requestInitiatorValues,
+    [name]: value,
+  });
+
+  if (name === "oid" || name === "inv") {
+    // ensure invoice and order id are the same
+    const orderId = requestInitiatorValues.oid;
+    const invoiceId = requestInitiatorValues.inv;
+
+    const orderIdInput = <HTMLInputElement>document.getElementById("id-oid");
+    const invoiceIdInput = <HTMLInputElement>document.getElementById("id-inv");
+
+    if (orderId > invoiceId) {
+      requestInitiatorValues.inv = <string>orderId;
+      invoiceIdInput.value = <string>orderId;
+    }
+
+    if (invoiceId > orderId) {
+      requestInitiatorValues.oid = <string>invoiceId;
+      orderIdInput.value = <string>invoiceId;
+    }
+  }
+};
 
 Object.keys(requestInitiatorValues).forEach((key: string) => {
   const group = document.createElement("div");
   group.className = "form-group";
 
-  const label = document.createElement("label");
+  const label = document.createElement("label")!;
   label.setAttribute("for", `id-${key}`);
   const labelText = document.createTextNode(key.toUpperCase());
   label.appendChild(labelText);
@@ -45,6 +75,9 @@ Object.keys(requestInitiatorValues).forEach((key: string) => {
   input.id = `id-${key}`;
   input.name = key;
   input.className = "form-control";
+  input.addEventListener("change", (event: Event) => {
+    onChange(event);
+  });
 
   group.appendChild(label);
   group.appendChild(input);
@@ -53,4 +86,27 @@ Object.keys(requestInitiatorValues).forEach((key: string) => {
   form.appendChild(submitBtn);
 });
 
-console.log("yeeey");
+const hashDataString = (dataString: string): string => {
+  return CryptoJS.HmacSHA1(dataString, HASH_KEY).toString();
+};
+
+const onSubmit = (): void => {
+  const dataStringObj: RequestInitiatorType = { ...requestInitiatorValues };
+
+  delete dataStringObj.hsh;
+
+  const dataString: string = Object.keys(dataStringObj)
+    .map((key: string): string => <string>dataStringObj[key])
+    .join("");
+
+  const hash: string = hashDataString(dataString);
+
+  const hashInput = <HTMLInputElement>document.getElementById("id-hsh")!;
+  hashInput.value = hash;
+};
+
+form.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+  onSubmit();
+  (<HTMLFormElement>event.target).submit();
+});
